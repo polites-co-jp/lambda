8fc9bb5ad64358cd2b0df54e3b86d146fcee9571
DESKTOP-GQNRGL6\kou <kou@example.com>
2021-11-10 13:29:34 +0900

    deploy完了まで

---
+++ src/functions/hello/handler.ts
@@ -0,0 +1,16 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
+import { formatJSONResponse } from "@libs/apiGateway";
+import { middyfy } from "@libs/lambda";
+
+import schema from "./schema";
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event
+) => {
+  return formatJSONResponse({
+    message: `はろわ!!`,
+    event,
+  });
+};
+
+export const main = middyfy(hello);
